900c8a751c908dd29f39a1cfaceb6d77cf8f9bed
Matt <mattmaliaros@gmail.com>
2019-11-14 20:24:14 -0500

    movie controller, lumen, etc

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+            // return Content("Hello World");
+            //return HttpNotFound();
+            //return EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+               pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
