f0312bb46193617a967dff8dd77b36575f4cd231
Matt <mattmaliaros@gmail.com>
2019-11-14 21:27:13 -0500

    MVC work

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,12 +14,30 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //Incorrect approaches Too complex
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+
+            var viewResult = new ViewResult();
+            return View(viewModel);
             // return Content("Hello World");
             //return HttpNotFound();
             //return EmptyResult();
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult Edit(int id)
         {
             return Content("id= " + id);
@@ -31,6 +50,10 @@ namespace Vidly.Controllers
             if (String.IsNullOrWhiteSpace(sortBy))
                 sortBy = "name";
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
